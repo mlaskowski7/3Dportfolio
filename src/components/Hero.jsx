495a533a1f18b8624b383b68907286dac59f59c1
Mateusz Laskowski <mlaskowski7>
2024-01-01 17:55:26 +0100

    about section, sectionWrapper implementation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,9 +17,27 @@ const Hero = () => {
           <h1 className={`${styles.heroHeadText}`}>Hi👋, My name is <span className='text-[#915eff]'>Mateusz</span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>I am a web dev, working in various back end <br className='sm:block hidden'/> and front end technologies.</p>
         </div>
-        
+
       </div>
       <ComputersCanvas />
+
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.dev 
+              animate={{
+                y: [0, 24, 0]
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: 'loop'
+              }}
+              className ="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
     </section>
   )
 }
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import { Suspense, useEffect, useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+import CanvasLoader from '../Loader';
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
+
+  const computer = useGLTF('./old_computer/scene.gltf')
+
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={0.6} groundColor="black" />
+      <pointLight intensity={10} />
+      <primitive 
+        object={computer.scene}
+        scale={isMobile ? 1 : 2}
+        position={[0,-3.25,-1.5]}
+        
+      />
+    </mesh>
   )
 }
 
-export default Computers
+const ComputersCanvas  = () =>{
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 500px)');
+    
+    setIsMobile(mediaQuery.matches)
+
+    const handleMediaQueryChange = (event) =>{
+      setIsMobile(event.matches);
+    }
+
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+    return () =>{
+      mediaQuery.removeEventListener('change', handleMediaQueryChange)
+    }
+    
+  },[])
+  
+  
+  return(
+    <Canvas 
+      frameLoop="demand" 
+      shadows 
+      camera={{position: [20,3,5], fov: 25}} 
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls 
+          enableZoom ={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers isMobile = {isMobile}/>
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  )
+}
+
+export default ComputersCanvas;
